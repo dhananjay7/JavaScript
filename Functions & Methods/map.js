7022e16c77eefe65980b9fe844e8be3c5eefd11d
Dhananjay Tayade <tayadedhananjay700@gmail.com>
2026-05-26 23:28:42 +0530

    Map function

---
+++ Functions & Methods/map.js
@@ -0,0 +1,10 @@
+// map is a array method which is the same as forEach what it does is it can create a new array with the old array after some operations. 
+ 
+
+let arr = [2,4,6,8];
+
+let newArr = arr.map ((val) => {
+    return val;
+});
+
+// Also it is the same properties like forEach for its value, position , array.
